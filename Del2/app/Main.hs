35a8920251b0738a78c785a507029ab3d64e4b21
Ragnhild Aalvik <raa009@student.uib.no>
2018-11-10 16:39:36 +0100

    Updated tasks + removed .stack-work files

--- Del2/app/Main.hs
+++ Del2/app/Main.hs
@@ -1,48 +1,51 @@
 module Main where 
 
-    import Client (visualise)
-    import Parser (parse)
-    import Types
-    
-    {-
-    For å starte visAST kaller du på funksjonen visualise :: String -> [Expr] -> IO (), slik:visualise username exprs
-    -}
-    
-    
-    -- Ikke rør denne, du skal jobbe med mainStep-funksjonen under. 
-    main = do
-        putStrLn "For å starte, skriv inn et uttrykk (feks + 2 3)"
-        str <- getLine 
-        case parse str of 
-            Nothing -> do 
-                putStrLn "Ugyldig uttrykk, prøv igjen."
-                main 
-            Just expr -> do 
-                mainStep expr
-    
-        
-    -- Oppgave 2.1 og 2.2
-    mainStep :: Expr -> IO ()
-    mainStep expr = undefined 
-    
-    
-    
-    {- eval
-    Evaluator som skal brukes når du skal visualisere med visAST. 
-    Denne kaller på takeOneStep helt til Expr er blitt et Num,
-    og returnerer listen med alle "tilstandene" Expr er i før den blir et Num (en verdi)
-    -}
-    eval :: Expr -> [Expr]
-    eval (Num num) = [Num num] 
-    eval expr = 
-        expr : eval (takeOneStep expr)
-    
-    
-    -- TODO: Lim inn implementasjonen av denne fra Del1
-    prettyPrint :: Expr -> IO () 
-    prettyPrint expr = undefined 
-    
-    
-    -- TODO: Lim inn implementasjonen av denne fra Del1
-    takeOneStep :: Expr -> Expr 
-    takeOneStep expr = undefined 
+import Data.Char
+import Client (visualise)
+import Types -- Denne importerer datatypen Expr, så du skal ikke lime den inn her 
+
+main = mainStep (Add (Num 2) (Num 3)) -- Default expression til å begynne med 
+
+
+-- Oppgave 1.4 og 2.1 - LIM INN IMPLEMENTASJONEN FRA DEL 1 HER, OG UTVID
+mainStep :: Expr -> IO ()
+mainStep expr = undefined 
+
+
+-- Oppgave 1.3 - LIM INN IMPLEMENTASJONEN FRA DEL 1 HER
+takeOneStep :: Expr -> Expr 
+takeOneStep expr = undefined 
+   
+
+-- Oppgave 1.2 - LIM INN IMPLEMENTASJONEN FRA DEL 1 HER
+prettyPrint :: Expr -> IO () 
+prettyPrint expr = undefined 
+
+
+-- Oppgave 1.1 - LIM INN IMPLEMENTASJONEN FRA DEL 1 HER (inkludert hjelpefunksjoner)
+parse :: String -> Expr 
+parse expr = undefined 
+
+
+------------ FERDIG IMPLEMENTERTE HJELPEFUNKSJONER (IKKE RØR) ------------------  
+
+{-
+Evaluator som skal brukes når du skal visualisere med visAST. 
+Denne kaller på takeOneStep helt til Expr er blitt et Num,
+og returnerer listen med alle "tilstandene" Expr er i før den blir et Num (en verdi)
+-}
+eval :: Expr -> [Expr]
+eval (Num num) = [Num num] 
+eval expr = 
+    expr : eval (takeOneStep expr)
+
+
+tokenize :: String -> [String]
+tokenize [] = [] 
+tokenize (' ':xs) = tokenize xs 
+tokenize s@(x:xs) 
+    | elem x "+*-" = [x] : tokenize xs 
+    | isDigit x = (takeWhile isDigit s) : tokenize (dropWhile isDigit s)
+tokenize ('i':'f':xs) = "if" : tokenize xs 
+tokenize ('t':'h':'e':'n':xs) = "then" : tokenize xs 
+tokenize ('e':'l':'s':'e':xs) = "else" : tokenize xs
--- Oblig2.hs
+++ Oblig2.hs
@@ -2,7 +2,6 @@ module Oblig2 where
 
 import Data.Char
 
-
 data Expr 
     = Num Int 
     | Add Expr Expr 
@@ -11,44 +10,31 @@ data Expr
     | If Expr Expr Expr 
     deriving (Show, Read)
 
---Du kan teste prettyPrint slik: prettyPrint (parse "+ 4 5")
+-- Du kan teste prettyPrint slik:
 main = prettyPrint (parse "+ 4 5")
 
 
--- Oppgave 1.1
+---------------------- Oppgave 1.1 ----------------------------------
+parse :: String -> Expr 
+parse str = undefined 
+
+
+---------------------- Oppgave 1.2 ----------------------------------
 prettyPrint :: Expr -> IO () 
 prettyPrint expr = undefined 
 
 
--- Oppgave 1.2 
+---------------------- Oppgave 1.3 ----------------------------------
 takeOneStep :: Expr -> Expr 
 takeOneStep expr = undefined 
 
 
-
-{- eval - ikke rør denne
-Evaluator som skal brukes når du skal visualisere med visAST. 
-Denne kaller på takeOneStep helt til Expr er blitt et Num,
-og returnerer listen med alle "tilstandene" Expr er i før den blir et Num (en verdi)
--}
-eval :: Expr -> [Expr]
-eval (Num num) = [Num num] 
-eval expr = 
-    expr : eval (takeOneStep expr)
-        
-
--------------------------- PARSER -----------------------
-parse :: String -> Expr 
-parse str = 
-    let 
-        tokens = tokenize str 
-    in 
-        case parseExpr tokens of 
-            (Just expr, []) -> 
-                expr 
-            _ -> error "Parsing failed" -- parsing failed somehow
+---------------------- Oppgave 1.4 ----------------------------------
+mainStep :: Expr -> IO ()
+mainStep expr = undefined 
 
 
+------------ FERDIG IMPLEMENTERTE HJELPEFUNKSJONER (IKKE RØR) ------------------  
 tokenize :: String -> [String]
 tokenize [] = [] 
 tokenize (' ':xs) = tokenize xs 
@@ -57,45 +43,4 @@ tokenize s@(x:xs)
     | isDigit x = (takeWhile isDigit s) : tokenize (dropWhile isDigit s)
 tokenize ('i':'f':xs) = "if" : tokenize xs 
 tokenize ('t':'h':'e':'n':xs) = "then" : tokenize xs 
-tokenize ('e':'l':'s':'e':xs) = "else" : tokenize xs 
-
-
-parseExpr :: [String] -> (Maybe Expr, [String])
-parseExpr (x:xs) | all isDigit x = 
-    (Just $ Num $ read x, xs)
-parseExpr ("+":xs) =
-    let 
-        (me1, rest) = parseExpr xs 
-        (me2, rest') = parseExpr rest 
-    in 
-        case (me1, me2) of 
-            (Just e1, Just e2) -> 
-                (Just $ Add e1 e2, rest')
-            _ -> (Nothing, [])
-parseExpr ("*":xs) =
-    let 
-        (me1, rest) = parseExpr xs 
-        (me2, rest') = parseExpr rest 
-    in 
-        case (me1, me2) of 
-            (Just e1, Just e2) -> 
-                (Just $ Mult e1 e2, rest')
-            _ -> (Nothing, [])
-parseExpr ("-":xs) = 
-    let 
-        (me, rest) = parseExpr xs 
-    in 
-        (fmap Neg me, rest)
-parseExpr ("if":xs) = 
-    let 
-        (meCond, restCond) = parseExpr xs                -- rest should start with "then"
-        (meThen, restThen) = parseExpr $ tail restCond   -- rest should start with "else"
-        (meElse, restElse) = parseExpr $ tail restThen
-    in 
-        case (meCond, meThen, meElse) of 
-            (Just eCond, Just eThen, Just eElse) ->
-                if head restCond == "then" && head restThen == "else" then 
-                    (Just $ If eCond eThen eElse, restElse)
-                else (Nothing, [])
-            _ -> (Nothing, [])
-parseExpr e = (Nothing, [])
+tokenize ('e':'l':'s':'e':xs) = "else" : tokenize xs
