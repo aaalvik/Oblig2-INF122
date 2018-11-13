0b6422039cbff9c4759b53b59c8792f9a0c812e6
Mikael Hanssen <hanssen.mikael@gmail.com>
2018-11-13 19:48:30 +0100

    Complete parser for Expr.
    - Includes checks for invalid syntax. Exceptions are thrown accordingly.
    - Tests needed to confirm task is completed

--- Oblig2.hs
+++ Oblig2.hs
@@ -1,7 +1,8 @@
 module Oblig2 where 
-    -- Mitt-UiB brukernavn:     [DITT BRUKERNAVN HER]
+    -- Mitt-UiB brukernavn:     nuc006
 
 import Data.Char
+import Data.List
 
 data Expr 
     = Num Int 
@@ -17,8 +18,35 @@ main = prettyPrint (parse "+ 4 5")
 
 ---------------------- Oppgave 1.1 ----------------------------------
 parse :: String -> Expr 
-parse str = undefined 
+-- Grammar uses prefix notation. Therefore, read from right to left
+parse str = parse' (reverse $ words str) []
 
+-- Reads input. Encountered sub-expressions are parsed, then added to the stack, before continuing.
+-- (The function is therefore tail-recursive).
+parse' :: [String] -> [Expr] -> Expr
+parse' [] [expr] = expr
+parse' [] stack = error $ "Unresolved expressions on stack: " ++ (show stack)
+parse' ("*":xs) (a:b:stack) = parse' xs ((Mult a b):stack)       -- Parse Mult, then push it onto the stack
+parse' ("+":xs) (a:b:stack) = parse' xs ((Add a b):stack)        -- Parse Add, then push it onto the stack
+parse' ("-":xs) (a:stack) = parse' xs ((Neg a):stack)            -- Parse Neg, then push it onto the stack
+parse' ("else":xs) stack = parseIf xs stack                      -- Parse if-statement
+parse' inp@(x:xs) stack
+    | all (isDigit) x = parse' xs ((Num (read x :: Int)):stack)  -- Parse number
+    | otherwise = error $ "Invalid syntax: " ++ (intersperse ' ' $ concat $ reverse inp)
+
+parseIf :: [String] -> [Expr] -> Expr
+parseIf xs (else':stack) = parse' xs'' ((If cond then' else'):stack)
+    where (then', xs') = parseUntil "then" xs
+          (cond, xs'') = parseUntil "if" xs'
+
+parseUntil :: String -> [String] -> (Expr,[String])
+parseUntil e xs = parseUntil' e xs []
+
+parseUntil' :: String -> [String] -> [String] -> (Expr,[String])
+parseUntil' e (x:xs) stack
+    | x == e = (parse' stack [], xs)
+    | otherwise = parseUntil' e xs (stack ++ [x])
+parseUntil' _ [] stack = (parse' stack [], [])
 
 ---------------------- Oppgave 1.2 ----------------------------------
 prettyPrint :: Expr -> IO () 
